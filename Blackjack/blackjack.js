0d04fd82337245e550b02323bd265a8aa8bcfc5f
theller5567 <theller5567@gmail.com>
2017-05-13 19:47:13 -0400

    finshed basic game functioning

--- Blackjack/blackjack.js
+++ Blackjack/blackjack.js
@@ -2,167 +2,184 @@ $(document).ready(function() {
     var deck = [];
     var dealerHand = [];
     var playerHand = [];
-    var playerPoints = 0;
-    var dealerPoints = 0;
+    var playerPoints;
+    var dealerPoints;
     var currentHand;
     var deckCount;
-    var countCards = true;
     var playerCash = 2500;
     var playerBet;
     var letsPlayBtn = $('#startGame');
     var dealBtn = $('#deal-button');
+    var hitBtn = $('#hit-button');
+    var standBtn = $('#stand-button');
     var hideDealer = true;
-
     letsPlayBtn.on('click', startGame);
-    dealBtn.on('click', function() {
-        dealPlayer();
-        dealDealer();
-    });
-
-    // Bet object
-    function Bets() {    
-        this.pot = 500;    
-        this.bet = 0;    
-        $('#bet').text(0);    
-        $('#pot').text('$' + this.pot);
-    }
-
-
-    // Deck
-    function Deck() {  
-        deck = [];  
-        deckCount = 0;  
-        if (countCards) { console.log("Shuffling Deck"); }   // Loop over each point value
-          
-        for (var points = 1;   points < 14; points++) {      
-            var suits = ['spades', 'hearts', 'clubs', 'diamonds'];       // Loop over each suit
-                  
-            for (var suit in suits) {           // Add each suit as an object to deck array
-                           deck.push(new Card(points, suits[suit]));       } // End suit for loop
-               } // End point for loop
-    }
-
-
-    // Draw card object
-    function drawCard(min, max) {   //set min amount of cards in deck
-           //set max amount of cards in deck
-          
-        var min = 1;  
-        var max = 52;   //use Math.random function using min and max values as parameters and assign to a variable name
-          
-        var randomNum = Math.floor(Math.random() * (min - max) * -1) + min;   //console.log(deck[randomNum]);
-           //return the random card from the deck
-          
-        return deck[randomNum];
-    }
-
-    // build card
-    function buildCard(cardObject) {
-        var cardObj = cardObject;
-        var pcards = [];
-        var dcards = [];
-        if (cardObj) {
-            if (cardObj.length == 1) {} else {
-                for (var i = 0; i < cardObj.length; i++) {
-                    var point = cardObj[i].point;
-                    var suit = cardObj[i].suit;
-                    var card = '<div class="card">';
-                    switch (point) {
-                        case 1:
-                            point = 'Ace';
-                            break;
-                        case 11:
-                            point = 'Jack';
-                            break;
-                        case 12:
-                            point = 'Queen';
-                            break;
-                        case 13:
-                            point = 'King';
-                            break;
-                    }
-                    card += '<div class="card-number">' + point + '</div>';
-                    card += '<div class="card-suit">' + suit + '</div>';
-
-                    if (cardObj[i].holder === 'player') {
-                        pcards.push(cardObj[i]);
-                        $('.player-cards').append(card);
-                    } else {
-                        dcards.push(cardObj[i]);
-                        $('.dealer-cards').append(card);
-                    }
-                }
-            }
-        }
-        if (1 < pcards.length) {
-          countPlayerCards(pcards);
-        }
-        if (1 < dcards.length) {
-          countDealerCards(dcards);
-        }
-    }
-
-
-    function welcome() {  
-        console.log('WELCOME TO BLACKJACK!');
-    }
-
+    dealBtn.on('click', deal);
+    hitBtn.on('click', hit);
+    standBtn.on('click', stand);
 
     function startGame() {
         $('.start-game-module').hide();
-        //popup saying welcome
-        welcome();
-        placeBets();
-        //create new deck
-        Deck();
-        //deal the cards
+        blackjack();
     }
 
-
-    function placeBets(bet) {
-        //if playerCash is greater than 0 continue function
-        //if bet amount is less than playerCash amount continue
-        //if(playerCash >= bet amount){
-        //playerCash = playerCash - bet amount
-        //}
-          
-        var betAmount = 10;
-        //   console.log(playerCash);
-          
-        if (playerCash > 0) {
-
-                
-            if (playerCash >= betAmount) {         playerCash -= betAmount;
-                //         console.log(playerCash);
-                     }   }
-        // dealPlayer();
-        // dealDealer();
-        // countCards();
+    function blackjack(){
+        this.init = function(){
+            console.log('Games has Started');
+        }(),
+        this.welcome = function(){  
+            console.log('WELCOME TO BLACKJACK!');
+        },
+        this.deck = [],
+        // Card Object
+        this.card = function(value, suit){    
+            this.point = value;    
+            this.suit = suit;
+        },
+        this.createDeck = function(){  
+            console.log("Shuffling Deck");
+            for(var points = 1; points < 14; points++){      
+                var suits = ['spades', 'hearts', 'clubs', 'diamonds']; 
+                for (var suit in suits) {          
+                    this.deck.push(new this.card(points, suits[suit])); 
+                }
+            }
+            deck = this.deck;
+        }();
     }
 
-
-    function dealPlayer() {   //Player gets two cards both face up.
-          
-        var firstCard = drawCard();  
-        var secondCard = drawCard();  
-        playerHand = [firstCard, secondCard];
-        playerHand[0].holder = 'player';
-        playerHand[1].holder = 'player';
-        buildCard(playerHand);
-
+    // Draw card object
+    function drawCard(min, max) { 
+        var min = 1;  
+        var max = 52;
+        var randomNum = Math.floor(Math.random() * (min - max) * -1) + min;   
+        //console.log('num rand: ', deck[randomNum]);
+        return deck[randomNum];
+    }
+    
+    function buildCard(cardObj){
+        if(cardObj){
+            var point = cardObj.point;
+            var suit = cardObj.suit;
+            var card = '<div class="card">';
+            switch(point) {
+                case 1:
+                    point = 'Ace';
+                    cardObj.value = 'Ace';
+                    break;
+                case 11:
+                    point = 'Jack';
+                    cardObj.value = 'Jack';
+                    break;
+                case 12:
+                    point = 'Queen';
+                    cardObj.value = 'Queen';
+                    break;
+                case 13:
+                    point = 'King';
+                    cardObj.value = 'King';
+                    break;
+                default:
+                    cardObj.value = cardObj.point;
+            }
+            card += '<div class="card-number">' + point + '</div>';
+            card += '<div class="card-suit">' + suit + '</div>';
+            if (cardObj.holder === 'player') {
+                $('.player-cards').append(card);
+                countCards(cardObj);
+            } else {
+                $('.dealer-cards').append(card);
+                countCards(cardObj);
+            }
+        }
+        
     }
 
-    function dealDealer() {
-        //give the Dealer two cards with one face down
-          
-        var firstCard = drawCard();  
-        var secondCard = drawCard();  
-        dealerHand = [firstCard, secondCard];
-        dealerHand[0].holder = 'dealer';
-        dealerHand[1].holder = 'dealer';
-        buildCard(dealerHand);
+    function countCards(){
+        if(blackjack.player.cards){
+            countPlayerCards();
+        }
+        if(blackjack.dealer.cards){
+            countDealerCards();
+        }
     }
 
+    function countPlayerCards(){
+        var playerSum = [];
+        var pcards = blackjack.player.cards;
+        for (var i = 0; i < pcards.length; i++) {
+            if (pcards[i].point > 10) {
+                pcards[i].point = 10;
+            }
+            playerSum.push(pcards[i].point);
+        }
+        blackjack.player.points = countArray(playerSum);
+        console.log('player-SUM: ', blackjack.player.points);
+        if(blackjack.player.points > 21){
+            lose();
+        }else if(blackjack.player.points == 21){
+            win();
+        }
+        $('.playerPoints').html('<h3>'+ blackjack.player.points +'</h3>');
+    }
+    function countDealerCards(){
+        var dealerSum = [];
+        var dcards = blackjack.dealer.cards;
+        for (var i = 0; i < dcards.length; i++) {
+            if (dcards[i].point > 10) {
+                dcards[i].point = 10;
+            }
+            dealerSum.push(dcards[i].point);
+        }
+        blackjack.dealer.points = countArray(dealerSum);
+        console.log('Dealer-SUM: ', blackjack.dealer.points);
+        if(blackjack.dealer.points > 21){
+            lose();
+        }else if(blackjack.dealer.points == 21){
+            win();
+        }
+        //$('.dealerPoints').html('<h3>'+ blackjack.dealer.points +'</h3>');
+    }
+
+    function Player(name){
+        this.name = name;
+        this.currency = 0;
+        this.cards = [];
+        this.points = 0;
+        this.turn = false;
+        this.holder = 'player';
+    }
+
+    function Dealer(){
+        this.name = 'Dealer';
+        this.cards = [];
+        this.cardsShowing = [];
+        this.points = 0;
+        this.turn = false;
+        this.holder = 'dealer';
+    }
+
+    function deal() {
+        dealBtn.css('display', 'none');
+        blackjack.player = new Player('Travis');
+        blackjack.dealer = new Dealer();
+        var card1 = drawCard();
+        var card2 = drawCard();
+        var carda = drawCard();
+        var cardb = drawCard();
+        card1.holder = 'player';
+        card2.holder = 'player';
+        carda.holder = 'dealer';
+        cardb.holder = 'dealer'; 
+        blackjack.player.cards = [card1, card2];
+        blackjack.dealer.cards = [carda, cardb];
+        $.each(blackjack.player.cards, function( index, card ) {
+          buildCard(card);
+        });
+        $.each(blackjack.dealer.cards, function( index, card ) {
+          buildCard(card);
+        });
+    }
 
     function countArray(array) {
         var sum = 0;
@@ -172,103 +189,51 @@ $(document).ready(function() {
         return sum;
     }
 
+    function hit(){
+        var newcard = drawCard();
+        console.log('NewCard: ',newcard);
+        newcard.holder = 'player';
+        blackjack.player.cards.push(newcard);
+        buildCard(newcard);
+    }
 
+    function stand(){
+        var playerPoints = blackjack.player.points;
+        var dealerPoints = blackjack.dealer.points;
 
-    function countPlayerCards(playersHand) {
-        var countSum = [];
-        if (playersHand) {
-            if (playersHand.length == 1) {} else {
-                for (var i = 0; i < playersHand.length; i++) {
-                    if (playersHand[i].point > 9) {
-                        playersHand[i].point = 10;
-                    }
-                    countSum.push(playersHand[i].point);
-                    if ((i + 1) == playerHand.length) {
-                        var toatalPoints = countArray(countSum);
-                        console.log('total points: ', toatalPoints);
-                        $('.playerPoints').html('<h4>' + toatalPoints + '</h4>');
-                        if (toatalPoints > 21) {
-                            loose();
-                        }
-                        if (toatalPoints == 21) {
-                            win();
-                        }
-                    }
-                }
-            }
+        if(playerPoints > dealerPoints){
+            win();
+            $('.dealer-cards').find('.card:nth-child(1)').addClass('show-card');
+        }else{
+            $('.dealer-cards').find('.card:nth-child(1)').addClass('show-card');
+            loose();
         }
     }
 
-
-
-    function countDealerCards(dealersHand) {
-        var countSum = [];
-        if (dealersHand) {
-            if (dealersHand.length == 1) {} else {
-                for (var i = 0; i < dealersHand.length; i++) {
-                    if (dealersHand[i].point > 9) {
-                        dealersHand[i].point = 10;
-                    }
-                    countSum.push(dealersHand[i].point);
-                    if ((i + 1) == playerHand.length) {
-                        var toatalPoints = countArray(countSum);
-                        console.log(toatalPoints);
-                        $('.dealerPoints').html('<h4>' + toatalPoints + '</h4>');
-                        if (toatalPoints > 21) {
-                            loose();
-                        }
-                        if (toatalPoints == 21) {
-                            win();
-                        }
-                    }
-                }
-            }
+    function placeBets(bet) {
+        var betAmount = 10;
+        if (playerCash > 0) {
+            if (playerCash >= betAmount) {         
+                playerCash -= betAmount;
+            }  
         }
     }
-
-
-    function hit() {
-        //if card points are less than 21 then pull random card from deck and add to player hand
-    }
-
 
     function stay() {
         //if card points are less than or equal to 21
     }
 
-
     function win() {
-        reset();
+        //reset();
+        console.log('win');
+        $('.win').addClass('show');
         startGame();
     }
-
 
     function lose() {
-        reset();
+        //reset();
+        console.log('loose');
+        $('.loose').addClass('show');
         startGame();
-    }
-
-
-    function flipDealerCard() {
-        //flip hole card from dealer
-        //count dealer card points
-        //if card points are less than 17, dealer draws another random card
-    }
-
-
-    function reset() {
-        // reset game values
-        // dealerHand = [];
-        // playerHand = [];
-        // playerPoints = 0;
-        // dealerPoints = 0;
-        // countCards = true;
-        // playerCash = 400;
-    }
-
-    // Card Object
-    function Card(value, suit) {    
-        this.point = value;    
-        this.suit = suit;
     }
 });
